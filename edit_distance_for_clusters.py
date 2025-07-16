9027d9e0d6efe15d2388c8c4f43461a1641bb023
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-07-16 09:38:47 +0900

    Finish first version of edit_distance_for_clusters.py

--- edit_distance_for_clusters.py
+++ edit_distance_for_clusters.py
@@ -11,7 +11,7 @@ import seaborn as sns
 import multiprocessing as mp
 import time
 import argparse, sys, os, random
-from data_loaders import load_cluster_dataset, VALID_CLUSTER_REGIONS
+from data_loaders import load_cluster_dataset, EQTimeWindows, VALID_CLUSTER_REGIONS, pkldump
 
 STRIDE=250 # Stride of the multiprocessed calculation
 RANDOMIZE_ARGS = True # Whether to calculate edit distances in a randomized way
@@ -32,11 +32,11 @@ parser.add_argument("--nthreads",
 parser.add_argument("--inputw",
         help="Size of the time window used for making predictions.",
         type=int,
-        default=7)
+        default=30)
 parser.add_argument("--outputw",
         help="Size of the time window for which a prediction is sought.",
         type=int,
-        default=1)
+        default=7)
 parser.add_argument("--tlambda",
         help="Multiplier of the lambda for timestamps.",
         type=float,
@@ -135,98 +135,7 @@ def editDistance(data1, data2, lambdas, lambdaDeletion=1):
 
     return M[row_ind,col_ind].sum()
 
-
-data = load_cluster_dataset(args.region, args.minmag)
-allDistMatrices = []
-
-allX_quakes = []
-allX_maxMag = []
-allX_meanMag = []
-allX_logN = []
-
-allX_quakes_N = []
-allX_quakes_logN = []
-#allX_seismicity = []
-
-allY_dayNumbers = []
-
-# Window size
-W = args.inputw
-
-# Prediction window
-PRED_WINDOW = args.outputw
-
-dayNumbers = data["day.number"] # To make things faster, save it in a variable
-maxDayNumber = (dt.datetime(2021, 8, 31) - dt.datetime(2000, 1, 1)).days
-# NOTE: day.number of 2000/01/01 is 0, so we do not need to add 1 to maxDayNumber
-
-for i in range(W-1, maxDayNumber + 1 - PRED_WINDOW):
-    quakeWindow = data[ (dayNumbers > i - W) * (dayNumbers <= i) ]
-    predWindow  = data[ (dayNumbers > i) * (dayNumbers <= i+PRED_WINDOW) ]
-    allY_dayNumbers.append(i + 1)
-
-    if len(quakeWindow) > 0:
-        quakeSequence = np.array(quakeWindow[["time.seconds", "magnitude", "longitude", "latitude", "depth"]])
-        quakeSequence[:,0] = quakeSequence[:,0] - (i-W+1) * 24 * 60 * 60
-    else:
-        quakeSequence = np.array([])
-
-    if len(predWindow) > 0:
-        allX_maxMag.append(predWindow["magnitude"].max())
-        allX_meanMag.append(predWindow["magnitude"].mean())
-    else:
-        allX_maxMag.append(0)
-        allX_meanMag.append(0)
-
-    allX_quakes.append(quakeSequence)
-    allX_logN.append(np.log(len(predWindow) + 1)) #variable to predict
-
-    allX_quakes_N.append(len(quakeSequence))
-    
-    allX_quakes_logN.append(np.log(len(quakeSequence) + 1))
-
-
-slic = data[data["year"] < 2011]
-
-timeStd      = np.std(np.diff(slic["time.seconds"])) * args.tlambda
-magnitudeStd = slic["magnitude"].std()
-depthStd     = slic["depth"].std()
-latitudeStd  = slic["latitude"].std()
-longitudeStd = slic["longitude"].std()
-
-baselineStds = [timeStd, magnitudeStd, longitudeStd, latitudeStd, depthStd]
-#baselineStds2 = [timeStd, magnitudeStd, (longitudeStd + latitudeStd) / 2, depthStd]
-
-baselineLambdas = [ 1 / i for i in baselineStds ]
-#baselineLambdas2 = [ 1 / i for i in baselineStds2 ]
-
-def calculateDistances(idx):
-    if idx % 50 == 0:
-        print(idx, end=" ")
-
-    N = len(allX_quakes)
-    distances = []
-
-    myX = allX_quakes[idx]
-    
-    for i in range(idx+1, N):
-        theirX = allX_quakes[i]
-        if args.dry_run and random.random() > 0.00005:
-            distances.append(-1)
-        else:
-            distances.append(editDistance(myX, theirX, baselineLambdas) if not args.dummy_dists else float(idx*10000 + i) )
-        #distances.append(editDistance2(myX, theirX, baselineLambdas2))
-    
-    return distances
-
-if args.partial:
-    valid_idxs = random.sample(range(len(allX_quakes) // 2), k=args.partial_n)
-    #print(sorted(valid_idxs))
-    valid_idxs = set(valid_idxs)
-else:
-    valid_idxs = set(range(len(allX_quakes)))
-
-def calculateDistances2(idx):
+def calculateDistances(idx, allX_quakes, valid_idxs):
     N = len(allX_quakes)
 
     #if idx % (STRIDE*500) == 0:
@@ -260,78 +169,90 @@ def calculateDistances2(idx):
     
     return idx, distances
 
-# allDistances = [ calculateDistances(i) for i in range(len(allX_quakes)) ]
 
-try:
-    mp.set_start_method('fork')
-except Exception: pass
+data = load_cluster_dataset(args.region, args.minmag)
+allDistMatrices = []
 
-# Deleting some variables here to minimize memory usage by child processes
-del data, slic
+for df in data:
+    eqtw = EQTimeWindows(df, args.inputw, args.outputw)
 
-allArgs = list(range(0,len(allX_quakes)**2,STRIDE))
+    baselineStds = eqtw.getBaselineStds(args.tlambda)
+    baselineLambdas = [ 1 / i for i in baselineStds ]
 
-if RANDOMIZE_ARGS:
-    allArgs_idxRand = list(range(len(allArgs))) 
-    np.random.shuffle(allArgs_idxRand) # 4 2 0 3 1
-    allArgs_idxRand_inv = np.zeros(len(allArgs), dtype=int)
-    for i in range(len(allArgs)):
-        allArgs_idxRand_inv[allArgs_idxRand[i]] = i  # 2 4 1 3 0
-    
-    allArgs = [ allArgs[i] for i in allArgs_idxRand ]
-
-beg = time.time()
-print("Beginning multiprocessed calculation.")
-with mp.Pool(args.nthreads) as p:
-    #allDistances = p.map(calculateDistances2, allArgs, chunksize=1)
-    allDistances = list(tqdm.tqdm(p.imap_unordered(calculateDistances2, allArgs, chunksize=1), total=len(allArgs), smoothing=0.1))
-end = time.time()
-print("Elapsed: ", end - beg)
-
-# if RANDOMIZE_ARGS:
-#     allArgs      = [ allArgs[i] for i in allArgs_idxRand_inv ]
-#     allDistances = [ allDistances[i] for i in allArgs_idxRand_inv ]
-
-N = len(allX_quakes)
-
-distanceMatrix = np.zeros(N**2)
-
-# for i, distances in zip(allArgs, allDistances):
-for i, distances in allDistances:
-    if len(distances) == STRIDE:
-        distanceMatrix[i:(i+STRIDE)] = distances
+    if args.partial:
+        valid_idxs = random.sample(range(len(eqtw.x_quakes) // 2), k=args.partial_n)
+        #print(sorted(valid_idxs))
+        valid_idxs = set(valid_idxs)
     else:
-        distanceMatrix[i:] = distances
+        valid_idxs = set(range(len(eqtw.x_quakes)))
 
-distanceMatrix = distanceMatrix.reshape(N, N)
-distanceMatrix = distanceMatrix + distanceMatrix.T # Complete the lower triangle
+    calculateDistances_d = lambda idx: calculateDistances(idx, eqtw.x_quakes)
 
-if args.dummy_dists:
-    correctMat = np.array([ [ j*10000 + i for i in range(N) ] for j in range(N) ])
-    correctMat[np.tril_indices(N)] = 0
-    correctMat[np.diag_indices(N)] = 0
-    correctMat = correctMat + correctMat.T
-    
-    if np.all(correctMat == distanceMatrix):
-        print("Distance matrix was filled correctly.")
-    else:
-        print("ERROR: Distance matrix was NOT filled correctly.")
+    # allDistances = [ calculateDistances(i) for i in range(len(allX_quakes)) ]
 
-# if args.partial:
-#     distanceMatrix = distanceMatrix[:,np.array([i for i in valid_idxs])]
+    try:
+        mp.set_start_method('fork')
+    except Exception: pass
 
+    allArgs = list(range(0,len(eqtw.x_quakes)**2,STRIDE))
+
+    if RANDOMIZE_ARGS:
+        allArgs_idxRand = list(range(len(allArgs))) 
+        np.random.shuffle(allArgs_idxRand) # 4 2 0 3 1
+        allArgs_idxRand_inv = np.zeros(len(allArgs), dtype=int)
+        for i in range(len(allArgs)):
+            allArgs_idxRand_inv[allArgs_idxRand[i]] = i  # 2 4 1 3 0
+        
+        allArgs = [ allArgs[i] for i in allArgs_idxRand ]
+
+    beg = time.time()
+    print("Beginning multiprocessed calculation.")
+    with mp.Pool(args.nthreads) as p:
+        #allDistances = p.map(calculateDistances2, allArgs, chunksize=1)
+        allDistances = list(tqdm.tqdm(p.imap_unordered(calculateDistances_d, allArgs, chunksize=1), total=len(allArgs), smoothing=0.1))
+    end = time.time()
+    print("Elapsed: ", end - beg)
+
+    N = len(eqtw.x_quakes)
+
+    distanceMatrix = np.zeros(N**2)
+
+    # for i, distances in zip(allArgs, allDistances):
+    for i, distances in allDistances:
+        if len(distances) == STRIDE:
+            distanceMatrix[i:(i+STRIDE)] = distances
+        else:
+            distanceMatrix[i:] = distances
+
+    distanceMatrix = distanceMatrix.reshape(N, N)
+    distanceMatrix = distanceMatrix + distanceMatrix.T # Complete the lower triangle
+
+    if args.dummy_dists:
+        correctMat = np.array([ [ j*10000 + i for i in range(N) ] for j in range(N) ])
+        correctMat[np.tril_indices(N)] = 0
+        correctMat[np.diag_indices(N)] = 0
+        correctMat = correctMat + correctMat.T
+        
+        if np.all(correctMat == distanceMatrix):
+            print("Distance matrix was filled correctly.")
+        else:
+            print("ERROR: Distance matrix was NOT filled correctly.")
+
+    allDistMatrices.append(distanceMatrix)
+
+# Once all matrices are obtained, we save it.
 try:
     # This might fail if the user choses an output directory that is in an external HD
-    fname = os.path.join(args.outdir, f"{EXPERIMENT_NAME}.npy")
+    fname = os.path.join(args.outdir, f"{EXPERIMENT_NAME}.pickle")
     if os.path.exists(fname):
         print("File already exists! Will preprend a random identifier to it.")
-        fname = os.path.join(args.outdir, f"{EXPERIMENT_NAME}-{os.urandom(4).hex()}.npy")
-    np.save(fname, distanceMatrix)
+        fname = os.path.join(args.outdir, f"{EXPERIMENT_NAME}-{os.urandom(4).hex()}.pickle")
+    pkldump(allDistMatrices, fname)
 except:
     # In that case, we save it in the current directory
     print("Could not save in the specified folder. Saving to current folder instead.")
-    fname = os.path.join("./", f"{EXPERIMENT_NAME}.npy")
+    fname = os.path.join("./", f"{EXPERIMENT_NAME}.pickle")
     if os.path.exists(fname):
         print("File already exists! Will preprend a random identifier to it.")
-        fname = os.path.join("./", f"{EXPERIMENT_NAME}-{os.urandom(4).hex()}.npy")
-    np.save(fname, distanceMatrix)
+        fname = os.path.join("./", f"{EXPERIMENT_NAME}-{os.urandom(4).hex()}.pickle")
+    pkldump(allDistMatrices, fname)
