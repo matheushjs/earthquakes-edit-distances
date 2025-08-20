954bba4ab711fb50d1b0050e0ffcdce3d307c6db
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-08-20 09:40:01 +0900

    Add correlation calculation

--- perform_forecasts.py
+++ perform_forecasts.py
@@ -68,6 +68,73 @@ def pklload(file):
     with open(file, "rb") as fp:
         return pickle.load(fp)
 
+def rbfPredict(y, distMat, trainSize, eps):
+    # Find the weights using only the training set
+    trainMat = distMat[:trainSize,:]
+    
+    trainGram = np.exp(-trainMat**2 / eps)
+
+    # Add bias parameters 
+    trainGram = np.hstack([ np.ones((trainGram.shape[0], 1)), trainGram ])
+
+    # Calculate weights
+    w = np.linalg.inv(trainGram.transpose() @ trainGram) @ (trainGram.transpose() @ y[:trainSize])
+
+    # Proceed to testing
+    testMat = distMat[trainSize:,:]
+
+    testGram = np.exp(-testMat**2 / eps)
+
+    # To plot results on train data
+    #plt.scatter(trainGram @ w, y[:trainSize])
+    
+    # Add bias parameters
+    testGram = np.hstack([ np.ones([testGram.shape[0], 1]), testGram ])
+
+    predicted = testGram @ w
+    
+    return predicted, y[trainSize:]
+
+def getExperiments(distMat, all_predictor, all_expected, eps, trainSize, numIter=100, numBases=100):
+    experiment = {
+        "correlation": [],
+        "predicted": []
+    }
+
+    for i in range(numIter):
+        print(i, end=" ")
+
+        selectIdx = np.arange(trainSize)
+        np.random.shuffle(selectIdx)
+        selectIdx = selectIdx[:numBases]
+
+        #MIN = 10
+        #MAX = 21
+        #candidates = np.int32(np.round(np.cumsum(np.random.uniform(MIN, MAX, size=trainSize // MIN))))
+        #candidates = candidates[candidates < trainSize]
+        #np.random.shuffle(candidates)
+        #selectIdx = candidates[:100] 
+
+        selectDistances = distMat[:,selectIdx]
+
+        predicted, real = rbfPredict(all_predictor, selectDistances, eps)
+
+        mmags = np.array(all_expected)[trainSize:]
+
+        # # Thresholds
+        # a = 6
+        # b = 3.2
+
+        # tp = np.sum((mmags >  a) * (predicted >  b))
+        # fp = np.sum((mmags <= a) * (predicted >  b))
+        # fn = np.sum((mmags >  a) * (predicted <= b))
+        # tn = np.sum((mmags <= a) * (predicted <= b))
+
+        experiment["correlation"].append(np.corrcoef(predicted, mmags)[0,1])
+        experiment["predicted"].append(predicted)
+    
+    return experiment
+
 data = load_dataset(args.region, args.minmag)
 eqtw = EQTimeWindows(data, args.inputw, args.outputw)
 x_maxMag = eqtw.x_maxMag
@@ -75,5 +142,10 @@ x_maxMag = eqtw.x_maxMag
 fname = os.path.join(args.dir, f"{EXPERIMENT_NAME}.npy")
 distanceMatrix = np.load(fname)
 
-print(x_maxMag)
-print(distanceMatrix)
+trainSize = distanceMatrix.shape[0] // 2
+trainMat = distanceMatrix[:trainSize,:]
+eps = 5 * np.mean(trainMat[trainMat > 0])**2
+
+experiment = getExperiments(distanceMatrix, eqtw.y_logN, x_maxMag, eps, trainSize, numIter=20)
+
+print(" ".join(experiment["correlation"]))
