a47ee36cf559539c45330936536ed1a0279eaf47
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-11-04 04:41:11 +0900

    Begin perform_forecasts_clusters.py

---
+++ perform_forecasts_clusters.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import datetime as dt
+import seaborn as sns
+import argparse, sys, os, random
+from data_loaders import load_cluster_dataset, VALID_REGIONS, EQTimeWindows
+import tqdm
+
+parser = argparse.ArgumentParser(prog='Calculates accuracy of prediction.')
+parser.add_argument("--region",
+        help="Region to use as predictor.",
+        default="ja",
+        choices=VALID_REGIONS)
+parser.add_argument("--pred-region",
+        help="Region to make predictions for.",
+        default="ja",
+        choices=VALID_REGIONS)
+parser.add_argument("--minmag",
+        help="Minimum magnitude.",
+        type=float,
+        default=0)
+parser.add_argument("--inputw",
+        help="Size of the time window used for making predictions.",
+        type=int,
+        default=7)
+parser.add_argument("--outputw",
+        help="Size of the time window for which a prediction is sought.",
+        type=int,
+        default=1)
+parser.add_argument("--tlambda",
+        help="Multiplier of the lambda for timestamps.",
+        type=float,
+        default=100.0)
+parser.add_argument("--limit-windows",
+        help="Limits the number of earthquakes in each window to the X-percentile of the window sizes distribution.",
+        action='store_true')
+parser.add_argument("--limit-windows-p",
+        help="Percentile to which window sizes should be truncated to.",
+        type=float,
+        default=0.8)
+parser.add_argument("--dir",
+        help="Read distance matrix from this directory.",
+        type=str,
+        default="./")
+args = parser.parse_args()
+
+print("Beginning program.")
+print("Command executed: {}".format(' '.join(sys.argv)))
+for i, j in args._get_kwargs():
+    print("{}: {}".format(i, j))
+print("==================")
+
