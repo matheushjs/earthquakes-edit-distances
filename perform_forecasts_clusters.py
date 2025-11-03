3669fe8832cff5d29123978b6749e4a86981f054
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2025-11-04 04:42:11 +0900

    Add RBF predictor

--- perform_forecasts_clusters.py
+++ perform_forecasts_clusters.py
@@ -81,3 +81,31 @@ def pkldump(obj, file):
 def pklload(file):
     with open(file, "rb") as fp:
         return pickle.load(fp)
+
+def rbfPredict(y, distMat, trainSize, eps):
+    # Find the weights using only the training set
+    trainMat = distMat[:trainSize,:]
+    
+    trainGram = np.exp(-trainMat**2 / eps)
+
+    # Add bias parameters 
+    trainGram = np.hstack([ np.ones((trainGram.shape[0], 1)), trainGram ])
+
+    # Calculate weights
+    w = np.linalg.inv(trainGram.transpose() @ trainGram) @ (trainGram.transpose() @ y[:trainSize])
+
+    # Proceed to testing
+    testMat = distMat[trainSize:,:]
+
+    testGram = np.exp(-testMat**2 / eps)
+
+    # To plot results on train data
+    #plt.scatter(trainGram @ w, y[:trainSize])
+    
+    # Add bias parameters
+    testGram = np.hstack([ np.ones([testGram.shape[0], 1]), testGram ])
+
+    predicted = testGram @ w
+    
+    return predicted, y[trainSize:]
+
